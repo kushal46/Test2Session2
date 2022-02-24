0f7bf8fde4b3f7bc4d28f1c0784badc55f6b6bca
kush46 <97089328+kushal46@users.noreply.github.com>
2022-02-24 10:00:40 +0000

    new

--- p3final.c
+++ p3final.c
@@ -1,1 +1,36 @@
+#include<stdio.h>
+#include<math.h>
+int input_number()
+{ int a;
+  printf("Enter the number: \n");
+  scanf("%d",&a);
+  return a;
+}
 
+int is_prime(int n)
+{   
+  for(int i=2;i<sqrt(n);i++)
+    {
+      if((n%i) == 0)
+        return 0;
+    }
+  return 1;
+}
+
+void output(int n,int prime)
+{
+  if(prime == 1)
+    printf("%d is prime",n);
+  else
+    printf("%d is not prime",n);
+}
+
+int main()
+{
+  int n,prime;
+  
+    n = input_number();
+    prime = is_prime(n);
+    output(n,prime);
+  return 0;
+}
--- p5final.c
+++ p5final.c
@@ -1,1 +1,17 @@
+#include<stdio.h>
+#include<math.h>
 
+int input_array_size()
+{
+  int a;
+  printf("Enter the size of the array: \n");
+  scanf("%d",&a);
+    return a;
+}
+void init_array(int n,inta[n])
+{
+  for(int 1=0;i<n;i++)
+    a[i]=i;
+    a[0]=0
+    a[1]=0
+}
